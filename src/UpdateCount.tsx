71b6b8fc7e176393e569e1f73ab16f66b0c58ecc
James Sinkler <jamessinkler713@gmail.com>
2021-06-04 18:44:15 -0700

    UpdateCount can + or - depending on props

---
+++ src/UpdateCount.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { StateContext } from './context';
+import {BasicAction} from "./reducer";
+
+interface InputProps {
+  sign: BasicAction["type"];
+  symbol: string;
+}
+
+const AddOrSubtract = ({sign, symbol}: InputProps) => {
+  const { dispatch } = useContext(StateContext)
+  return (
+    <div>
+      <p onClick={()=> dispatch({ type: sign})} >{symbol}</p>
+    </div>
+  )
+};
+export default AddOrSubtract;
